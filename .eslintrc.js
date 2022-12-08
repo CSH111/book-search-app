691926f3836061d2a3012601cc39edecb264d867
CSH111 <seongho6022@naver.com>
2022-12-08 18:29:44 +0900

    chore: 의존성 패키지 설치, eslint 설정

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "simple-import-sort"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+
+    // "prettier",
+  ],
+  rules: {
+    "simple-import-sort/imports": 1,
+    "simple-import-sort/exports": 1,
+    "@typescript-eslint/no-unused-vars": 1,
+  },
+};
+
+// "plugin:simple-import-sort/recommended",
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es2021: true,
+//     node: true,
+//   },
+//   extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+
+//   overrides: [],
+//   parserOptions: {
+//     ecmaVersion: "latest",
+//     sourceType: "module",
+//   },
+//   plugins: ["react", "simple-import-sort"],
+//   rules: {
+//     "simple-import-sort/imports": "error",
+//     "simple-import-sort/exports": "error",
+//     "react/react-in-jsx-scope": "off",
+//     "react/prop-types": 0,
+//     "no-unused-vars": 0,
+//   },
+// };
